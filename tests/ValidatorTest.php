6f989317e475b7e1b9e4c68c7109531c73a66ef3
ddabout <ddabout@users.noreply.github.com>
2016-09-24 03:56:06 +0100

    Create ValidatorTest.php

---
+++ tests/ValidatorTest.php
@@ -0,0 +1,16 @@
+<?php
+
+require_one __DIR__ . "/../Validator.php";
+
+class ValidatorTest extends PHPUnit_Framework_TestCase{
+
+  /**
+   * @test
+   */
+  public function method_required(){
+  
+    $this->assertTrue( true );
+    
+  }
+
+}
